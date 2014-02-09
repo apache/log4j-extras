12e519428e0c2650d3487274531e240ffe4dc682
Gary D. Gregory <ggregory@apache.org>
2014-02-09 03:38:32 +0000

    Fix formating.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/companions/extras/trunk@1566201 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/log4j/rolling/helper/FileRenameAction.java
+++ src/main/java/org/apache/log4j/rolling/helper/FileRenameAction.java
@@ -19,64 +19,60 @@ package org.apache.log4j.rolling.helper;
 
 import java.io.File;
 
-
 /**
  * File rename action.
  *
  * @author Curt Arnold
  */
 public final class FileRenameAction extends ActionBase {
-  /**
-   * Source.
-   */
-  private final File source;
+    /**
+     * Source.
+     */
+    private final File source;
 
-  /**
-   * Destination.
-   */
-  private final File destination;
+    /**
+     * Destination.
+     */
+    private final File destination;
 
-  /**
-   * If true, rename empty files, otherwise delete empty files.
-   */
-  private final boolean renameEmptyFiles;
+    /**
+     * If true, rename empty files, otherwise delete empty files.
+     */
+    private final boolean renameEmptyFiles;
 
-  /**
-   * Creates an FileRenameAction.
-   *
-   * @param src current file name.
-   * @param dst new file name.
-   * @param renameEmptyFiles if true, rename file even if empty, otherwise delete empty files.
-   */
-  public FileRenameAction(
-    final File src, final File dst, boolean renameEmptyFiles) {
-    source = src;
-    destination = dst;
-    this.renameEmptyFiles = renameEmptyFiles;
-  }
-
-  /**
-   * Rename file.
-   *
-   * @return true if successfully renamed.
-   */
-  public boolean execute() {
-    return execute(source, destination, renameEmptyFiles);
-  }
-
-  /**
-   * Rename file.
-   * @param source current file name.
-   * @param destination new file name.
-   * @param renameEmptyFiles if true, rename file even if empty, otherwise delete empty files.
-   * @return true if successfully renamed.
-   */
-  public static boolean execute(
-    final File source, final File destination, boolean renameEmptyFiles) {
-    if (renameEmptyFiles || source.length() > 0) {
-      return source.renameTo(destination);
+    /**
+     * Creates an FileRenameAction.
+     *
+     * @param src current file name.
+     * @param dst new file name.
+     * @param renameEmptyFiles if true, rename file even if empty, otherwise delete empty files.
+     */
+    public FileRenameAction(final File src, final File dst, boolean renameEmptyFiles) {
+        source = src;
+        destination = dst;
+        this.renameEmptyFiles = renameEmptyFiles;
     }
 
-    return source.delete();
-  }
+    /**
+     * Rename file.
+     *
+     * @return true if successfully renamed.
+     */
+    public boolean execute() {
+        return execute(source, destination, renameEmptyFiles);
+    }
+
+    /**
+     * Rename file.
+     * @param source current file name.
+     * @param destination new file name.
+     * @param renameEmptyFiles if true, rename file even if empty, otherwise delete empty files.
+     * @return true if successfully renamed.
+     */
+    public static boolean execute(final File source, final File destination, boolean renameEmptyFiles) {
+        if (renameEmptyFiles || source.length() > 0) {
+            return source.renameTo(destination);
+        }
+        return source.delete();
+    }
 }
